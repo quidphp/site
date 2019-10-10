37c0cd3f78352cff301bb2a64fdb48a805818226
emondpph <emondpph@gmail.com>
2019-10-10 14:55:55 +0000

    Apply fixes from StyleCI

--- src/App/Error.php
+++ src/App/Error.php
@@ -1,5 +1,12 @@
 <?php
 declare(strict_types=1);
+
+/*
+ * This file is part of the QuidPHP package.
+ * Website: https://quidphp.com
+ * License: https://github.com/quidphp/site/blob/master/LICENSE
+ */
+
 namespace Quid\Site\App;
 use Quid\Core;
 
