e0ec8cee22d3c06d6b2ac5819eb27c5aa509196b
Pierre-Philippe Emond <emondpph@gmail.com>
2019-11-21 16:37:34 +0100

    Apply fixes from StyleCI (#1)

--- src/App/CliClearCache.php
+++ src/App/CliClearCache.php
@@ -3,12 +3,13 @@ declare(strict_types=1);
 
 /*
  * This file is part of the QuidPHP package.
+ * Author: Pierre-Philippe Emond <emondpph@gmail.com>
  * Website: https://quidphp.com
- * License: https://github.com/quidphp/lemur/blob/master/LICENSE
+ * License: https://github.com/quidphp/site/blob/master/LICENSE
+ * Readme: https://github.com/quidphp/site/blob/master/README.md
  */
 
 namespace Quid\Site\App;
-use Quid\Base;
 use Quid\Base\Cli;
 use Quid\Core;
 
